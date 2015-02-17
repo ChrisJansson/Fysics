0b3cf3d3443532fdcb866a53f9b739b18a77f738
Chris Jansson <chris.janssom@gmail.com>
2015-02-17 21:21:57 +0100

    Lets get the ball rolling with some vector math

---
+++ Console/Program.fs
@@ -0,0 +1,9 @@
+﻿open vector
+
+[<EntryPoint>]
+let main argv =
+    let a = { x = 1.0; y = 2.0; z = 3.0 } 
+    let b = { x = 2.0; y = 3.0; z = 4.0 }
+    crossProduct a b
+    |> printfn "%A"
+    0 // return an integer exit code
---
+++ Console/vector.fs
@@ -0,0 +1,27 @@
+﻿module vector
+
+    type vector3 = 
+        {   x : float
+            y : float
+            z : float }
+
+        static member (+) (a, b) =
+            {   x = a.x + b.x
+                y = a.y + b.x
+                z = a.z + b.z }
+
+        static member (*) (s, v) =
+            {   x = s * v.x
+                y = s * v.y
+                z = s * v.z }
+
+    let dotProduct a b =
+        a.x * b.x +
+        a.y * b.y +
+        a.z * b.z
+
+    let crossProduct a b =
+        {   x = a.y * b.z - a.z * b.y 
+            y = a.z * b.x - a.x * b.z
+            z = a.x * b.y - a.y * b.x }
+
