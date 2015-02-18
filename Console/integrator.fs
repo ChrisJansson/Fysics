8cdd4496f75afdc718490370f37353a9a04d1182
Chris Jansson <chris.janssom@gmail.com>
2015-02-18 18:57:55 +0100

    Integrate multiple particles

--- Console/integrator.fs
+++ Console/integrator.fs
@@ -1,12 +1,15 @@
 ﻿module integrator
-open vector
-open particle
-    let integrate (dt : float) (p : particle) =
+    open vector
+    open particle
+
+    let integrate dt p =
         let nextPosition = dt * p.position
         let nextVelocity = dt * p.acceleration + p.velocity
         let dampedVelocity = p.damping ** dt * nextVelocity
-        { p with position = nextPosition; velocity = nextPosition }
+        { 
+            p with position = nextPosition; velocity = nextPosition 
+        };
 
-
-
-
+    let integrateAll dt particles =
+        particles
+        |> Seq.map (integrate dt)
