a9f77b2f2fba291b99de1efd2945aa8d0d448059
Chris Jansson <chris.janssom@gmail.com>
2015-02-19 19:42:17 +0100

    Draw quad instead

--- Console/Program.fs
+++ Console/Program.fs
@@ -24,12 +24,14 @@ type FysicsWindow() =
         GL.LoadIdentity()
         GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0)
 
-        GL.Begin(PrimitiveType.Triangles)
+        GL.Begin(PrimitiveType.Quads)
 
         GL.Color3(Color.MidnightBlue)
         GL.Vertex2(-1.0f, 1.0f)
         GL.Color3(Color.SpringGreen)
-        GL.Vertex2(0.0f, -1.0f)
+        GL.Vertex2(-1.0f, -1.0f)
+        GL.Color3(Color.SpringGreen)
+        GL.Vertex2(1.0f, -1.0f)
         GL.Color3(Color.Ivory)
         GL.Vertex2(1.0f, 1.0f)
 
