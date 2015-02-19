248331e242c49d7addc3aead9d11d432d8f5db41
Chris Jansson <chris.janssom@gmail.com>
2015-02-19 16:57:56 +0100

    Ported the first OpenTK sample

--- Console/Program.fs
+++ Console/Program.fs
@@ -1,9 +1,44 @@
 ﻿open vector
+open OpenTK
+open OpenTK.Input
+open OpenTK.Graphics.OpenGL
+open System.Drawing
+
+type FysicsWindow() = 
+    inherit GameWindow()
+
+    override this.OnLoad(e) =
+        this.VSync <- VSyncMode.On
+
+    override this.OnUpdateFrame(e) =
+        let a = base.Keyboard.[OpenTK.Input.Key.Escape]
+        if a then do
+            this.Exit()
+
+    override this.OnResize(e) =
+        GL.Viewport(0, 0, this.Width, this.Height)
+
+    override this.OnRenderFrame(e) =
+        GL.Clear(ClearBufferMask.ColorBufferBit ||| ClearBufferMask.DepthBufferBit)
+        GL.MatrixMode(MatrixMode.Projection)
+        GL.LoadIdentity()
+        GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0)
+
+        GL.Begin(PrimitiveType.Triangles)
+
+        GL.Color3(Color.MidnightBlue)
+        GL.Vertex2(-1.0f, 1.0f)
+        GL.Color3(Color.SpringGreen)
+        GL.Vertex2(0.0f, -1.0f)
+        GL.Color3(Color.Ivory)
+        GL.Vertex2(1.0f, 1.0f)
+
+        GL.End();
+
+        this.SwapBuffers();
 
 [<EntryPoint>]
 let main argv =
-    let a = { x = 1.0; y = 2.0; z = 3.0 } 
-    let b = { x = 2.0; y = 3.0; z = 4.0 }
-    crossProduct a b
-    |> printfn "%A"
+    let window = new FysicsWindow()
+    window.Run(60.0)
     0 // return an integer exit code
